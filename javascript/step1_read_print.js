4879845bc51959282b593d0b4ccca71be44d0e5b
Khalid Jebbari <khalid.jebbari@externe.sacem.fr>
2015-03-26 13:53:45 +0100

    Start step1

---
+++ javascript/step1_read_print.js
@@ -0,0 +1,33 @@
+function READ(x) {
+    return x;
+}
+
+function EVAL(x) {
+    return x;
+}
+
+function PRINT(x) {
+    return x;
+}
+
+function rep(x) {
+    return PRINT(EVAL(READ(x)));
+}
+
+var readline = require("readline"),
+    rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        terminal: false
+    });
+
+rl.setPrompt("user> ");
+rl.prompt();
+
+rl.on("line", function(line) {
+    process.stdout.write(rep(line));
+    rl.prompt();
+}).on("exit", function() {
+    rl.close();
+    process.exit(0);
+});
